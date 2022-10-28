ab8a14ec7e13ad71bb7ee2feefe4608b46c7305f
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-10-28 11:45:35 +0530

    Add files via upload

---
+++ AssignQ5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class AssignQ5
+{
+  static void Main()
+    {
+        double r, Area;
+        const double PI = 3.14;
+        Console.WriteLine("Program to calculate area and circumference of circle");
+        Console.WriteLine("\nEnter the radius of radius of circle");
+        r = Convert.ToDouble(Console.ReadLine());
+        Area = PI * r * r;
+        Console.WriteLine("\nThe area of circle is {0}", Area);
+        Console.WriteLine("\nThe circumference of circle is {0}", 2 * PI * r);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
