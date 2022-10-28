6a413c4687c59089d9793be6fa3d66dc01067c89
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-10-28 11:44:37 +0530

    Add files via upload

---
+++ AssignQ2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class AssignQ2
+{
+  static void Main()
+    {
+        Console.WriteLine("Average marks of 5 students");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = Convert.ToInt32(Console.ReadLine());
+        int e = Convert.ToInt32(Console.ReadLine());
+        int[] average = { a, b, c, d, e };
+        Console.WriteLine("Maximum marks obtained are", +average.Max());
+        Console.ReadLine();
+
+
+
+    }  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
