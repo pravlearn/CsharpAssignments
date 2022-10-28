3358ab8e555b9ea9bfefa4d1aa5a15b5cacaec4b
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-10-28 11:46:46 +0530

    Add files via upload

---
+++ Assign8Q.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssignments1
+{
+    internal class Assign8Q
+    {
+      static void Main()
+        {
+            int a, b;
+            Console.WriteLine("Enter two numbers:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                Console.WriteLine("{0} is the largest number", a);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the largest number", b);
+
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
