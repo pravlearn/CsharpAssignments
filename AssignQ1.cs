38c9b42f02e6ef7b9ee4e517159f96f570587127
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-10-28 11:44:00 +0530

    Add files via upload

---
+++ AssignQ1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class AssignQ1
+{
+  static void Main()
+  {
+        Console.WriteLine("Enter a number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter another number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0} + {1} ={2}", num1, num2, num1 + num2);
+        Console.WriteLine("{0} - {1} ={2}", num1, num2, num1 - num2);
+        Console.WriteLine("{0} * {1} ={2}", num1, num2, num1 * num2);
+        Console.WriteLine("{0} / {1} ={2}", num1, num2, num1 / num2);
+        Console.WriteLine("{0} % {1} ={2}", num1, num2, num1 % num2);
+
+
+
+
+
+
+
+    }    
+
+
+
+
+
+
+
+
+
+
+
+
+}
