a29609bd3475b47b6faeb97ecb816028eb1386d9
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-10-28 11:46:24 +0530

    Add files via upload

---
+++ AssignQ7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssignments1
+{
+    internal class AssignQ7
+    {
+      static void Main()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i <= 10);
+
+        }
+
+
+
+
+    }
+}
