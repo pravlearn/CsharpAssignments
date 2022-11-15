866138f1e2010b3fdd9b190341d7346f6ce57b53
pravlearn <115153400+pravlearn@users.noreply.github.com>
2022-11-15 05:46:18 +0000

    Add files via upload

---
+++ FileIO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+
+namespace assignment5
+{
+    class directory
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            string root = @"C:\ Temp";
+            string subdir = @"C:\Temp\Pravallika";
+            Directory.CreateDirectory(root);
+            Directory.CreateDirectory(subdir);
+            path = "C:\\Temp\\Pravallika";
+            try
+            {
+                File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+
+
+
+
+        }
+    }
+
+
+
+}
